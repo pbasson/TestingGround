ce6cceeed0560602eb969e2b979a742691ca9c83
Preetpal Basson <preet.basson@gmail.com>
2019-03-11 11:10:27 +0000

    V0.010:  Update

--- Source/TestingGround/ChooseWaypoint.cpp
+++ Source/TestingGround/ChooseWaypoint.cpp
@@ -2,13 +2,26 @@
 
 #include "ChooseWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
+
 
 
 EBTNodeResult::Type UChooseWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	//UE_LOG(LogTemp, Warning, TEXT("AI C++"));
+	//Get Patrol Points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoint = PatrollingGuard->PatrolPoints;
+
+	//Set Next Waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoint[Index]);
+
+	auto NextIndex = (Index + 1) % PatrolPoint.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 
 	return EBTNodeResult::Succeeded;
 }
--- Source/TestingGround/PatrollingGuard.h
+++ Source/TestingGround/PatrollingGuard.h
@@ -14,6 +14,7 @@ class TESTINGGROUND_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
+public:
 		UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
 		TArray<AActor*> PatrolPoints;
 
