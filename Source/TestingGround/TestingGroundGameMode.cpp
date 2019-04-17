669e280a7be13786a6c70db2aebe39f7af9c006a
Preetpal.Basson <preetpal.basson@mastek.com>
2019-04-17 15:23:35 +0100

    v0.055: Refactor Update

--- Source/TestingGround/TestingGroundGameMode.cpp
+++ Source/TestingGround/TestingGroundGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundGameMode::ATestingGroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/NPC/TP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
