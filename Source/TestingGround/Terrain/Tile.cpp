14632a9695020a9cba743521f59db9b8747229a2
Preetpal.Basson <preetpal.basson@mastek.com>
2019-05-09 14:51:50 +0100

    v0.079: Update

--- Source/TestingGround/Terrain/Tile.cpp
+++ Source/TestingGround/Terrain/Tile.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tile.h"
+#include "Engine/World.h"
 
 // Sets default values
 ATile::ATile()
@@ -26,10 +27,18 @@ void ATile::Tick(float DeltaTime)
 }
 
 
-void ATile::PlaceActors()
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
 {
 	FVector Min(0, -2000, 0);
 	FVector Max(4000, 2000, 0);
 	FBox Bounds(Min, Max);
-	FVector SpawnPoint =  FMath::RandPointInBox(Bounds);
+
+	int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (size_t i = 0; i < NumberToSpawn; i++)
+	{
+		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
+		AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+		Spawned->SetActorRelativeLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+	}
 }
